962e86034d573935126421695c4837202bded840
alessio-zamparelli <37191190+alessio-zamparelli@users.noreply.github.com>
2019-03-16 11:36:27 +0100

    done point 1

--- Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -4,10 +4,9 @@ package it.polito.tdp.alien;
  * Sample Skeleton for 'Alien.fxml' Controller Class
  */
 
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,40 +15,104 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
-	
-    @FXML
-    private ResourceBundle resources;
-    @FXML
-    private URL location;
-    @FXML
-    private TextField txtWord;
-    @FXML
-    private TextArea txtResult;
-    @FXML
-    private Button btnTranslate;
-    @FXML
-    private Button btnReset;
-        
-    
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-    	assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
-    	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
-    	
-    }
-  
-    
-    @FXML
-    void doTranslate(ActionEvent event) {
-    	    	
-    }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
 
-    }
-    
+	AlienDictionary dict = new AlienDictionary();
+	private String pattern = "^(\\w+(\\ \\w+)*)$";
+
+	@FXML
+	private ResourceBundle resources;
+	@FXML
+	private URL location;
+	@FXML
+	private TextField txtWord;
+	@FXML
+	private TextArea txtResult;
+	@FXML
+	private Button btnTranslate;
+	@FXML
+	private Button btnReset;
+
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
+		assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
+		assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
+
+	}
+
+	@FXML
+	void doTranslate(ActionEvent event) {
+
+		// la parola in ingresso è nel formato:
+		// <parola aliena> <traduzione> per inserirne una nuova
+		// <parola aliena> per cercarne solo la traduzione
+
+		String newTranslation = txtWord.getText().toLowerCase();
+
+		// controllo la validità in ingresso
+		if (!newTranslation.matches(pattern) || newTranslation == null || newTranslation.length() == 0) {
+			txtResult.appendText("Testo in ingresso non valido, controllare la formattazione\n");
+			txtResult.appendText("Le possibilità sono:\n");
+			txtResult.appendText("<parola aliena> <traduzione>  	per inserirne una nuova\n");
+			txtResult.appendText("<parola aliena> 					per cercarne solo la traduzione\n");
+			return;
+		}
+
+		
+		StringTokenizer st = new StringTokenizer(newTranslation, " ");
+		
+		if (!st.hasMoreElements()) {
+			txtResult.appendText("Inserire una o due parole.\n");
+			return;
+		}
+		
+		//controllo che si abbiano al piu 2 elementi
+		if (st.countTokens()>2) {
+			txtResult.appendText("Inserire al piu 2 termini\n");
+			return;
+		}
+
+		String alienWord = st.nextToken();
+
+		//se devo inserire la traduzione
+		if(st.hasMoreElements()) {
+			String translation = st.nextToken();
+			
+			//controllo formattazione
+			if (!alienWord.matches("[a-zA-Z]*") || !translation.matches("[a-zA-Z]*")) {
+				txtResult.appendText("Inserire solo caratteri alfabetici.\n");
+				return;
+			}
+			
+			dict.addWord(alienWord, translation);
+			txtResult.appendText(String.format("Aggiunto al dizionario %s che significa %s\n", alienWord, translation));
+		} else {
+			
+			// se devo tradurre una parola
+			if (!alienWord.matches("[a-zA-Z]*")) {
+				txtResult.appendText("Inserire solo caratteri alfabetici.\n");
+				return;
+			}
+			
+			String translation = dict.translateWord(alienWord);
+			if (translation == null) {
+				txtResult.appendText(String.format("La parola %s non è ancora stata inserita!\n", alienWord));
+			} else {
+				txtResult.appendText(String.format("La parola %s significa %s\n", alienWord, translation));
+			}
+		}
+		
+		txtWord.clear();
+		
+		
+	}
+
+	@FXML
+	void doReset(ActionEvent event) {
+		txtResult.clear();
+		txtWord.clear();
+		dict.reset();
+	}
+
 }
--- Lab02_Alien/src/it/polito/tdp/alien/Test.java
+++ Lab02_Alien/src/it/polito/tdp/alien/Test.java
@@ -2,4 +2,21 @@ package it.polito.tdp.alien;
 
 public class Test {
 
+	public static void main(String[] args) {
+		AlienDictionary dict = new AlienDictionary();
+		dict.addWord("allala", "fanculo");
+		System.out.println("Cerco allala -> " + dict.translateWord("allala"));
+
+
+		dict.addWord("allala", "fanculoPanzone");		
+		System.out.println("Cerco allala -> " + dict.translateWord("allala"));
+		System.out.println("Cerco fannanna -> " + dict.translateWord("fannanna"));
+
+		Word w1 = new Word("alien", "ita");
+		Word w2 = new Word("alien", "merd");
+		System.out.println(w1.getTranslation());
+		System.out.println(w2.getTranslation());
+		
+		System.out.println("Uguali? " + w1.equals(w2));
+	}
 }
